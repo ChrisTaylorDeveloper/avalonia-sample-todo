679d2379470551defb65e863e8df2dd02e878791
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-11-28 18:17:10 +0000

    Working point.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,13 @@
 ﻿namespace GetStartedApp.ViewModels;
 
+using System.Collections.ObjectModel;
+
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
+
+    /// <summary>
+    /// Gets a collection of <see cref="ToDoItem"/> which allows adding and removing items
+    /// </summary>
+    public ObservableCollection<ToDoItemViewModel> ToDoItems { get; } = new ObservableCollection<ToDoItemViewModel>();
 }
